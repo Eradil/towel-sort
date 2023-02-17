2ba187fc8713c98a8d9b91bbae31e7e4279bae6d
Eradil <eradil.abasbekov.ss@gmail.comgit config --global user.name Eradil git config --global user.name Eradil git config --global user.email eradil.abasbekov.ss@gmail.com>
2023-02-17 22:16:34 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let res = [];
+    if (!matrix) {
+        return [];
+    } else {
+        matrix.forEach((element, index) => {
+            if (index % 2 !== 0) {
+                console.log(element.reverse());
+            }
+            for (let num of element) {
+                res.push(num);
+            }
+        });
+    }
+
+    return res;
+};
